106f15bf01c734676486762c9e51f616c765ae2f
d.zavrin <d.zavrin@leadgid.ru>
2023-04-12 20:10:22 +0400

    feat: MAIN-T-6 space config
    Signed-off-by: d.zavrin <d.zavrin@leadgid.ru>

--- .space.kts
+++ .space.kts
@@ -4,6 +4,26 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("Build and test") {
+    container("amazoncorretto:17-alpine") {
+        shellScript {
+            content = "./gradlew build"
+        }
+    }
+}
+
+job("Flyway migration") {
+    container("amazoncorretto:17-alpine") {
+        shellScript {
+            content = "./gradlew flywayMigrate"
+        }
+    }
+}
+
+job("Detekt static code analysis") {
+    container("amazoncorretto:17-alpine") {
+        shellScript {
+            content = "./gradlew detekt"
+        }
+    }
 }
