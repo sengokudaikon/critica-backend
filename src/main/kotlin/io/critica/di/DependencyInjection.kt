fd3ac42a609ddd3587e4f4afcb667cde862be8cf
d.zavrin <d.zavrin@leadgid.ru>
2023-04-12 04:50:45 +0400

    feat: MAIN-T-12 updated basic config
    Signed-off-by: d.zavrin <d.zavrin@leadgid.ru>

--- src/main/kotlin/io/critica/di/DependencyInjection.kt
+++ src/main/kotlin/io/critica/di/DependencyInjection.kt
@@ -4,6 +4,7 @@ import io.critica.config.AppConfig
 import io.critica.persistence.repository.LobbyRepository
 import io.critica.presentation.action.lobby.Lobby
 import io.critica.presentation.controller.LobbyController
+import io.github.cdimascio.dotenv.dotenv
 import org.koin.dsl.module
 
 val appModule = module {
@@ -11,4 +12,15 @@ val appModule = module {
     single { LobbyRepository() }
     single { Lobby(get()) }
     single { LobbyController(get()) }
+}
+
+val dotenv = dotenv {
+    directory = "./"
+    if (System.getenv("ENV") == "production") {
+        filename = ".env.production"
+    } else if (System.getenv("ENV") == "dev") {
+        filename = ".env.development"
+    } else {
+        filename = ".env.local"
+    }
 }
