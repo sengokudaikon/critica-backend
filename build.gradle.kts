d1d3c5e778dc7d3d809342f2734945fd27659f78
d.zavrin <d.zavrin@leadgid.ru>
2023-04-12 16:39:24 +0400

    feat: MAIN-T-12 added static page
    Signed-off-by: d.zavrin <d.zavrin@leadgid.ru>

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
     implementation("io.swagger.codegen.v3:swagger-codegen-generators:1.0.38")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
+    implementation("io.ktor:ktor-server-host-common-jvm:2.2.4")
+    implementation("io.ktor:ktor-server-status-pages-jvm:2.2.4")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/io/critica/Application.kt
+++ src/main/kotlin/io/critica/Application.kt
@@ -3,9 +3,13 @@ package io.critica
 import io.critica.config.AppConfig
 import io.critica.di.*
 import io.critica.presentation.controller.LobbyController
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
@@ -30,8 +34,20 @@ fun Application.main() {
     configureSockets()
     configureRouting()
 
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+        }
+    }
+
+
     val lobbyController: LobbyController by inject()
     routing {
+        static("/static") {
+            resources("static")
+        }
+
+        get("/") { call.respondRedirect("/static/index.html") }
         route("/api") {
             this@routing.lobbyRoutes(lobbyController)
         }
