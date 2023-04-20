a9b34b0d11838785e52e4d95a384621b69a40f05
Daniil Zavrin <cryogen.sxu@gmail.com>
2023-04-20 18:51:32 +0400

    feat: dev update health
    Signed-off-by: Daniil Zavrin <cryogen.sxu@gmail.com>

--- src/main/kotlin/io/critica/Application.kt
+++ src/main/kotlin/io/critica/Application.kt
@@ -42,7 +42,7 @@ private const val DURATION: Long = 10L
 private const val WS_DURATION: Long = 15L
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::main)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::main)
         .start(wait = true)
 
 }
